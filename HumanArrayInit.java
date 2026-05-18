30187be7e46acd76d9e6681a07b36ac617f765cf
Yoh Kobayashi <y.kobayashi.415@stn.nitech.ac.jp>
2026-05-18 16:45:11 +0900

    update HumanArralnit2

--- HumanArrayInit.java
+++ HumanArrayInit.java
@@ -3,13 +3,13 @@ class HumanArrayInit{
     public static void main(String [] args){
         //初期化
         Human[] a={
-            new Human("丈司",160,50),
+            new Human("丈司",180,50),
             new Human("家",170,60),
             };
         //代入
         Human[] b=new Human[2];
-        b=new Human[]{new Human("丈司",165,55),
-        new Human("家",175,65)
+        b=new Human[]{new Human("丈司",180,55),
+        new Human("家",175,55)
         };
 
         //表示
